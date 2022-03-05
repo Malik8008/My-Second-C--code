6655e65e060716e7e9fa42b380c9cc3ca394681f
Malik8008 <tu60prbs1@code.edu.az>
2022-03-05 12:40:46 +0400

    task1 added

---
+++ My Third code/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace My_Third_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Hasbullah", "Hardy", "Miraga", "Eli", "John" };
+
+            foreach (string name in names)
+            {
+                int count = 0;
+                foreach (char character in name)
+                {
+                    count++;
+                }
+                if (count > 5)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
